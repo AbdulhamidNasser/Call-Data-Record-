4281f60efcfe829d570279daee6a9299391f47b0
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-06-21 11:28:33 +0400

    add controller class and repositry notation

--- src/main/java/com/codeline/Call/Data/Record/Repository/CdrsRepository.java
+++ src/main/java/com/codeline/Call/Data/Record/Repository/CdrsRepository.java
@@ -2,6 +2,8 @@ package com.codeline.Call.Data.Record.Repository;
 
 import com.codeline.Call.Data.Record.Model.Cdrs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CdrsRepository extends JpaRepository<Cdrs,Long> {
 }
