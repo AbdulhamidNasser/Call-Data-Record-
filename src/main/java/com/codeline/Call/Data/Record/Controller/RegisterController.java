1f73e5b7f004903c5e8e1976d8d45cfabeb17466
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-06-21 15:43:17 +0400

    handle User Registration

--- src/main/java/com/codeline/Call/Data/Record/Controller/RegisterController.java
+++ src/main/java/com/codeline/Call/Data/Record/Controller/RegisterController.java
@@ -1,7 +1,33 @@
 package com.codeline.Call.Data.Record.Controller;
 
+import com.codeline.Call.Data.Record.Model.Register;
+import com.codeline.Call.Data.Record.RequestObj.RegisterRequestObj;
+import com.codeline.Call.Data.Record.RequestObj.ReportRequestObj;
+import com.codeline.Call.Data.Record.Servise.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterController {
+    @Autowired
+    RegisterService registerService;
+    @PostMapping("/api/register")
+    public ResponseEntity<Void> registerUser (@RequestBody RegisterRequestObj registerRequestObj) {
+        createUser(registerRequestObj);
+        return ResponseEntity.ok().build();
+
+    }
+    public void createUser(RegisterRequestObj registerRequestObj){
+        Register registerObj = new Register();
+        registerObj.setEmail(registerRequestObj.getEmail());
+        registerObj.setPassword(registerRequestObj.getPassword());
+        registerObj.setUsername(registerRequestObj.getUsername());
+        registerService.register(registerObj);
+    }
+
+
 }
+
--- src/main/java/com/codeline/Call/Data/Record/Servise/RegisterService.java
+++ src/main/java/com/codeline/Call/Data/Record/Servise/RegisterService.java
@@ -1,7 +1,17 @@
 package com.codeline.Call.Data.Record.Servise;
 
+import com.codeline.Call.Data.Record.Model.Register;
+import com.codeline.Call.Data.Record.Repository.RegisterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
+    @Autowired
+    RegisterRepository registerRepository;
+    public void register(Register register) {
+        registerRepository.save(register);
+    }
+
+
 }
