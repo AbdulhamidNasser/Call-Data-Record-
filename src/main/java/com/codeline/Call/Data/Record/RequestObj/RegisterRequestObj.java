23f15dbbae21353885538bd4a787861886eb5acd
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-06-21 14:41:12 +0400

    RegisterRequestObj class

--- src/main/java/com/codeline/Call/Data/Record/RequestObj/RegisterRequestObj.java
+++ src/main/java/com/codeline/Call/Data/Record/RequestObj/RegisterRequestObj.java
@@ -1,4 +1,17 @@
 package com.codeline.Call.Data.Record.RequestObj;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class RegisterRequestObj {
+    String username;
+    String password;
+    String email;
 }
