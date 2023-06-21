6d71bd02e0087ae22244c3b9525813df2f1db6c0
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-06-21 14:54:59 +0400

    CdrsResponseObj class

--- src/main/java/com/codeline/Call/Data/Record/RequestObj/CdrsRequestObj.java
+++ src/main/java/com/codeline/Call/Data/Record/RequestObj/CdrsRequestObj.java
@@ -1,4 +1,18 @@
 package com.codeline.Call.Data.Record.RequestObj;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CdrsRequestObj {
+
+    String callerNumber;
+    String receiverNumber;
+    int duration;
+    String timestamp;
 }
--- src/main/java/com/codeline/Call/Data/Record/ResponseObj/CdrsResponseObj.java
+++ src/main/java/com/codeline/Call/Data/Record/ResponseObj/CdrsResponseObj.java
@@ -1,4 +1,19 @@
 package com.codeline.Call.Data.Record.ResponseObj;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CdrsResponseObj {
+    long id;
+    String callerNumber;
+    String receiverNumber;
+    int duration;
+    String timestamp;
 }
